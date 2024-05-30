679e18fe2f63aaa8a4cbcaa299b0005fef78e1fb
tathingocanh <anhta6416@gmail.com>
2024-05-30 10:57:02 +0700

    function

---
+++ function.py
@@ -0,0 +1,22 @@
+def anh():
+	pass
+	print('ngocanh')
+anh()
+
+def a():
+	b=10
+	c=20
+	d=c + b 
+	print(d)
+a()
+def b(a,b):
+	print (a)
+	print (b)
+b('anh','18 tui')
+
+def f(kteam=[]):
+	kteam.append('f')
+	print(kteam)
+f()
+f()
+f()
