06efed0ba1599a139332f7814dffd5b75fc28b28
tathingocanh <anhta6416@gmail.com>
2024-06-04 07:57:37 +0700

    function

--- function.py
+++ function.py
@@ -42,3 +42,58 @@ def b(**a):
 	for key, value in a.items():
 		print(key,'=>', value)
 b(ten='anh',tuoi=18)
+
+
+def make_slogan():
+	global kteam
+	kteam = 'Howkteam'
+make_slogan()
+print(kteam)
+
+def make_global():
+	global x
+	x=1
+def local():
+	x=5
+	print('x=',x)
+make_global()
+print(x)
+local()
+print(x)
+
+def cal_rec_per(width,height):
+	per=(width+height)*2
+	return per 
+rec_1_width=5
+rec_1_height=3
+rec_1_per=cal_rec_per(rec_1_width,rec_1_height)
+print(rec_1_per)
+print(cal_rec_per(7,4))
+
+def gen():
+	while True:
+		x=yield
+		yield x**2
+g=gen()
+print(next(g))
+print(g.send(2))
+print(next(g))
+print(g.send(10))
+
+
+#lambda
+ave= lambda a,b,c: (a+b+c)/3
+print(ave(1,2,3))
+a=[lambda x:x**2,lambda x: x**3]
+print(a[1](2))
+for i in a:
+	print(i(3))
+
+a=[1,2,3,4]
+print(list(map(lambda x:x+1,a)))
+
+tong=lambda x,y:x+y
+a=[1,2,3,4]
+b=[5,6,7,8]
+c=(map(tong,a,b))
+print(list(c))
